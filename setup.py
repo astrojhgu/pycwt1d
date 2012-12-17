4d49653bfe4a282f4032baa974c18993806ef166
astrojhgu <astrojhgu@gmail.com>
2012-12-18 01:31:59 +0800

    renamed:    cwt_filter.py -> pycwt1d/cwt_filter.py 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+from distutils.core import setup, Extension
+
+if sys.version_info.major==3:
+    boost_python_lib='boost_python3'
+else:
+    boost_python_lib='boost_python'
+
+module1=Extension('cwt1d',
+                  include_dirs=['../cwt1d/','../fftw_blitz'],
+                  libraries=[boost_python_lib,'gsl','gslcblas','fftw3f','fftw3_threads','fftw3f_threads'],
+                  #library_dirs=['/usr/include/lib'],
+                  sources=['../fftw_blitz/fftw_blitz.cpp','pycwt1d.cpp'])
+
+setup(name='pycwt1d.core',
+      package='pycwt1d',
+      version='1.0',
+      ext_modules=[module1])
